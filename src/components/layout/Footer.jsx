19efa111da57003c3eaea14a36eab6f69a516003
VladTsar <vladtsar1873@gmail.com>
2022-11-23 05:51:35 +0200

    deployment

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,16 @@
+import { FaGithub } from 'react-icons/fa'
+
+function Footer() {
+    const footerYear = new Date().getFullYear()
+
+    return (
+        <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
+            <div>
+                <FaGithub />
+                <p>Copyright &copy; {footerYear} All rights reserved</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
